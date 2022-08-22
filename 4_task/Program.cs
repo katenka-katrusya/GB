0241c71f8a6ea449adc35a558de53556ba08d391
SEkaterina <katerina-kozyrenko@yandex.ru>
2022-08-22 08:10:54 +0300

    task4

---
+++ 4_task/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 4. Напишите программу, кот. принимает на вход три числа и выдаёт максимальное из этих чисел
+
+int numberA = new Random().Next(1, 100); // 1 2 3 4 ... 99
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100);
+Console.WriteLine(numberB);
+int numberC = new Random().Next(1, 100);
+Console.WriteLine(numberC);
+int max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+Console.Write($"Максимально число = {max}");
