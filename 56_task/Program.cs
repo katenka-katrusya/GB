70a0473398aed978a7319a5320dbe6a968253ea3
SEkaterina <katerina-kozyrenko@yandex.ru>
2022-09-02 09:49:40 +0300

    task56

---
+++ 56_task/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] res = new int[rows, columns];
+
+    for (int i = 0; i < res.GetLength(0); i++)
+    {
+        for (int j = 0; j < res.GetLength(1); j++)
+        {
+            res[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return res;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindMinSumRow(int[,] array)
+{
+    int minSum = 0;
+    int minIndexRow = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (i == 0) 
+        {
+            minSum = sum;
+            minIndexRow = i + 1;
+        }
+        else if (sum < minSum)
+        {
+            minSum = sum;
+            minIndexRow = i + 1;
+        }
+    }   
+    Console.WriteLine($"Минимальная сумма элементов в {minIndexRow} строке = {minSum}");
+}
+
+int[,] myArray = GetArray(5, 4, 0, 9);
+PrintArray(myArray);
+FindMinSumRow(myArray);
